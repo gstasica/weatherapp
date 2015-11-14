17d4f8cfd681675de9a7c0cacc74aca9eebea05c
gstasica@gmail.com <gstasica@gmail.com>
2015-11-14 19:34:49 +0000

    adding service to pass data between pages

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
 
+//ROUTES
 weatherApp.config(function($routeProvider){
 
 	$routeProvider
@@ -10,13 +11,34 @@ weatherApp.config(function($routeProvider){
 		.when('/forecast', {
 			templateUrl : 'pages/forecast.htm',
 			controller: 'forecastCtrl'
+		})
+		.otherwise( {
+			templateUrl:'pages/home.htm',
+			controller: 'homeCtrl'
 		});
 });
 
-weatherApp.controller('homeCtrl', ['$scope', function($scope){
-	$scope.city='';
+//SERVICES
+weatherApp.service('forecastService', function(){
+
+	var self = this;
+	this.city = '';
+
+	// this.getCity = function(){
+	// 	return self.city;
+	// };
+});
+
+//CONTROLLERS
+weatherApp.controller('homeCtrl', ['$scope','forecastService', function($scope,forecastService){
+	 $scope.city = forecastService.city;
+
+	 $scope.$watch('city', function(){
+	 	forecastService.city = $scope.city;
+	 });
+
 }]);
 
-weatherApp.controller('forecastCtrl', ['$scope', function($scope){
-	$scope.city='';
+weatherApp.controller('forecastCtrl', ['$scope','forecastService', function($scope, forecastService){
+	$scope.city=forecastService.city;
 }]);
